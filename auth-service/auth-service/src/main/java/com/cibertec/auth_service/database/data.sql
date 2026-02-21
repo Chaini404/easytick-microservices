652365534780b0cc4dcb798364d608ca35986dc6
Angely <eusebioangely@gmail.com>
2026-02-21 18:25:48 -0500

    data de auth

--- auth-service/auth-service/src/main/java/com/cibertec/auth_service/database/data.sql
+++ auth-service/auth-service/src/main/java/com/cibertec/auth_service/database/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO users (name,email,password,role,enabled) VALUES
-('Ana Torres','ana@mail.com','123','USER',true),
-('Luis Perez','luis@mail.com','123','USER',true),
-('Carlos Diaz','carlos@mail.com','123','ADMIN',true),
-('Maria Lopez','maria@mail.com','123','USER',true),
-('Sofia Ramos','sofia@mail.com','123','USER',true),
-('Pedro Silva','pedro@mail.com','123','USER',true),
-('Valeria Cruz','valeria@mail.com','123','USER',true),
-('Jorge Mena','jorge@mail.com','123','USER',true),
-('Elena Ruiz','elena@mail.com','123','USER',true),
-('Diego Soto','diego@mail.com','123','USER',true);
+INSERT INTO users (name,email,password,role_type,enabled) VALUES
+('Ana Torres','ana@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Luis Perez','luis@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','STAFF',true),
+('Carlos Diaz','carlos@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','ADMIN',true),
+('Maria Lopez','maria@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Sofia Ramos','sofia@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Pedro Silva','pedro@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Valeria Cruz','valeria@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Jorge Mena','jorge@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Elena Ruiz','elena@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','CUSTOMER',true),
+('Diego Soto','diego@mail.com','$2a$10$7QJvQ4vQ1Q8Z8YzF3zQpEOTQ6vJzJ1y6RkWwM9zJXQmYkLkZp7G2K','ORGANIZER',true);
